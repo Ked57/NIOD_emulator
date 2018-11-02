ff31faa34d5478be3485e2a139a61f12a3eb7220
Ked57 <kedketh@gmail.com>
2018-11-02 15:11:51 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const net = require("net");
+const PORT = 15488;
+const HOST = "127.0.0.1";
+
+const dataHandler = require("./data_handler/data_handler");
+
+console.log("starting NIOD_emulator");
+const server = net.createServer(socket => {
+  console.log(`New client from ${socket.remoteAddress}`);
+  socket.on("data", data => {
+    console.log(data);
+    dataHandler.handleData(data);
+  });
+});
+
+server.on("error", e => {
+  if (e.code === "EADDRINUSE") {
+    console.log("Address in use, retrying...");
+    setTimeout(() => {
+      server.close();
+      server.listen(PORT, HOST);
+    }, 1000);
+  }
+});
+
+server.listen(PORT, HOST);
+console.log(`Server started, listening on ${HOST}:${PORT}`);
